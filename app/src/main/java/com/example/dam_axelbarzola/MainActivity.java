2e247d85ba2741ea4a32bfbffb25a9cf36b0ef62
Axel <20b3010455@untels.edu.pe>
2024-05-15 10:12:02 -0500

    Proyecto terminado

--- app/src/main/java/com/example/dam_axelbarzola/MainActivity.java
+++ app/src/main/java/com/example/dam_axelbarzola/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dam_axelbarzola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button_enviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
     }
 }
